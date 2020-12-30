4515c596417d619b10f36284dcf5c375473b0a20
DenisRomanovsky <denys.r@stadiumgoods.com>
2020-12-30 15:19:41 +0300

    [NO-TASK] Linter integration

--- src/App.jsx
+++ src/App.jsx
@@ -8,43 +8,53 @@ import AccountSelector from './components/AccountSelector/AccountSelector';
 import BalanceUser from './components/BalanceUser/BalanceUser';
 import BalanceAnnotation from './components/BalanceAnnotation/BalanceAnnotation';
 
-function App () {
-  const [accountAddress, setAccountAddress] = useState(null);
-  const { apiState, keyringState, apiError } = useSubstrate();
+function App() {
+	const [accountAddress, setAccountAddress] = useState(null);
+	const { apiState, keyringState, apiError } = useSubstrate();
 
-  const loader = (text) =>
-    <Dimmer active>
-      <Loader size='small'>{text}</Loader>
-    </Dimmer>;
+	const loader = (text) => (
+		<Dimmer active>
+			<Loader size='small'>{text}</Loader>
+		</Dimmer>
+	);
 
-  const message = (err) =>
-    <Grid centered columns={1} padded>
-      <Grid.Column>
-        <Message negative compact floating
-          header='Error Connecting to Substrate'
-          content={`${err}`}
-        />
-      </Grid.Column>
-    </Grid>;
+	const message = (err) => (
+		<Grid centered columns={1} padded>
+			<Grid.Column>
+				<Message
+					negative
+					compact
+					floating
+					header='Error Connecting to Substrate'
+					content={`${err}`}
+				/>
+			</Grid.Column>
+		</Grid>
+	);
 
-  if (apiState === 'ERROR') return message(apiError);
-  else if (apiState !== 'READY') return loader('Connecting to Substrate');
+	if (apiState === 'ERROR') return message(apiError);
+	else if (apiState !== 'READY') return loader('Connecting to Substrate');
 
-  if (keyringState !== 'READY') {
-    return loader('Loading accounts (please review any extension\'s authorization)');
-  }
+	if (keyringState !== 'READY') {
+		return loader(
+			"Loading accounts (please review any extension's authorization)"
+		);
+	}
 
-  const contextRef = createRef();
+	const contextRef = createRef();
 
-  return (
-    <div ref={contextRef}>
-      <Sticky context={contextRef}>
-        <AccountSelector account={accountAddress} onChange={setAccountAddress} />
-      </Sticky>
-      <BalanceAnnotation account={accountAddress} />
-      <BalanceUser account={accountAddress} />
-    </div>
-  );
+	return (
+		<div ref={contextRef}>
+			<Sticky context={contextRef}>
+				<AccountSelector
+					account={accountAddress}
+					onChange={setAccountAddress}
+				/>
+			</Sticky>
+			<BalanceAnnotation account={accountAddress} />
+			<BalanceUser account={accountAddress} />
+		</div>
+	);
 }
 
 export default App;
--- src/components/BalanceUser/BalanceUser.jsx
+++ src/components/BalanceUser/BalanceUser.jsx
@@ -2,53 +2,68 @@ import React, { useState, useEffect } from 'react';
 import { useSubstrate } from '../../substrate-lib';
 import { Table, Grid } from 'semantic-ui-react';
 
-function BalanceUser ({ account }) {
-  const { api } = useSubstrate();
-  const [currencyBalance, setCurrencyBalance] = useState({});
-  const currencies = ['MINT', 'DOT', 'KSM', 'BTC', 'ETH', 'MDOT', 'MKSM', 'MBTC', 'METH'];
+function BalanceUser({ account }) {
+	const { api } = useSubstrate();
+	const [currencyBalance, setCurrencyBalance] = useState({});
+	const currencies = [
+		'MINT',
+		'DOT',
+		'KSM',
+		'BTC',
+		'ETH',
+		'MDOT',
+		'MKSM',
+		'MBTC',
+		'METH',
+	];
 
-  useEffect(() => {
-    let unsubscribeAll = null;
-    const currencyBalanceTemp = {};
-    const fetchData = async () => {
-      for (const currency of currencies) {
-        const data = await api.query.tokens.accounts(account, currency);
-        !currencyBalanceTemp[account] && (currencyBalanceTemp[account] = []);
-        currencyBalanceTemp[account].push({
-          currency: currency,
-          balance: account ? data.free.toHuman() : '0'
-        });
-      }
-      setCurrencyBalance(currencyBalanceTemp);
-      return () => unsubscribeAll();
-    };
-    fetchData()
-      .then(unsub => {
-        unsubscribeAll = unsub;
-      })
-      .catch(console.error);
-  }, [api.query.tokens, currencies, account]);
+	useEffect(() => {
+		let unsubscribeAll = null;
+		const currencyBalanceTemp = {};
+		const fetchData = async () => {
+			for (const currency of currencies) {
+				const data = await api.query.tokens.accounts(account, currency);
+				!currencyBalanceTemp[account] && (currencyBalanceTemp[account] = []);
+				currencyBalanceTemp[account].push({
+					currency: currency,
+					balance: account ? data.free.toHuman() : '0',
+				});
+			}
+			setCurrencyBalance(currencyBalanceTemp);
+			return () => unsubscribeAll();
+		};
+		fetchData()
+			.then((unsub) => {
+				unsubscribeAll = unsub;
+			})
+			.catch(console.error);
+	}, [api.query.tokens, currencies, account]);
 
-  return (
-    <Grid.Column>
-      <h1>Balance User</h1>
-      <Table celled striped size='small'>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell key="headerAsset">Asset</Table.HeaderCell>
-            <Table.HeaderCell key="headerBalance">Balance</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {(currencyBalance[account] || []).map((balance) =>
-            <Table.Row key={balance.currency}>
-              <Table.Cell key={`currency-${balance.currency}`}>{balance.currency}</Table.Cell>
-              <Table.Cell key={`balance-${balance.balance}`}>{balance.balance}</Table.Cell>
-            </Table.Row>)}
-        </Table.Body>
-      </Table>
-    </Grid.Column>
-  );
+	return (
+		<Grid.Column>
+			<h1>Balance User</h1>
+			<Table celled striped size='small'>
+				<Table.Header>
+					<Table.Row>
+						<Table.HeaderCell key='headerAsset'>Asset</Table.HeaderCell>
+						<Table.HeaderCell key='headerBalance'>Balance</Table.HeaderCell>
+					</Table.Row>
+				</Table.Header>
+				<Table.Body>
+					{(currencyBalance[account] || []).map((balance) => (
+						<Table.Row key={balance.currency}>
+							<Table.Cell key={`currency-${balance.currency}`}>
+								{balance.currency}
+							</Table.Cell>
+							<Table.Cell key={`balance-${balance.balance}`}>
+								{balance.balance}
+							</Table.Cell>
+						</Table.Row>
+					))}
+				</Table.Body>
+			</Table>
+		</Grid.Column>
+	);
 }
 
 export default BalanceUser;
