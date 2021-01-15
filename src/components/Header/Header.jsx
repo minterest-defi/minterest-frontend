46a068e849d8f422995828760747bb49b8fbd099
Polina Liutsko <polina.liutsko@gmail.com>
2021-01-15 13:50:45 +0200

    [FE-127] Added new component Logo.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import AccountSelector from './AccountSelector/AccountSelector';
 import BalanceAnnotation from './BalanceAnnotation/BalanceAnnotation';
+import Logo from './Logo/Logo';
 
 function Header({ account, onChange }) {
 	return (
 		<div>
+			<Logo />
 			<AccountSelector account={account} onChange={onChange} />
 			<BalanceAnnotation account={account} />
 		</div>
