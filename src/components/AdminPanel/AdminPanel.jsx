974e10a2c5e341c021470abf28bfdf1465bba2a0
Polina Liutsko <polina.liutsko@gmail.com>
2021-01-19 17:46:20 +0200

    [FE-58] Added new component InsuranseDeposit.

--- src/components/AdminPanel/AdminPanel.jsx
+++ src/components/AdminPanel/AdminPanel.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import InsuranceDeposit from './Insurance/InsuranceDeposit/InsuranceDeposit';
 import SwitchAnnotation from './SwitchAnnotation/SwitchAnnotation';
 import SwitchPauseOperation from './SwitchPauseOperation/SwitchPauseOperation';
 
-function AdminPanel({ account }) {
+function AdminPanel({ account, onChange, poolState }) {
 	return (
 		<div>
 			<SwitchPauseOperation account={account} />
 			<SwitchAnnotation account={account} />
+			<InsuranceDeposit
+				account={account}
+				onChange={onChange}
+				poolState={poolState}
+			/>
 		</div>
 	);
 }
