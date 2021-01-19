6b64730bd9c65bfb0b378b5bd56bf58afcbf8971
Polina Liutsko <polina.liutsko@gmail.com>
2021-01-19 15:33:02 +0200

    [FE-71] Added new component SwitchAnnotation.

--- src/components/AdminPanel/AdminPanel.jsx
+++ src/components/AdminPanel/AdminPanel.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import SwitchAnnotation from './SwitchAnnotation/SwitchAnnotation';
 import SwitchPauseOperation from './SwitchPauseOperation/SwitchPauseOperation';
 
 function AdminPanel({ account }) {
 	return (
 		<div>
 			<SwitchPauseOperation account={account} />
+			<SwitchAnnotation account={account} />
 		</div>
 	);
 }
