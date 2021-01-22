0fffd82426e902b426a8f482b28b8a9aff8be667
Polina <polina.liutsko@gmail.com>
2021-01-22 11:04:03 +0200

    PL/FE-158 Show Admin the insurance total for pool. (#16)
    * [FE-158] The functionality of the new TotalInsurance component has been implemented..
    
    * [FE-158] Changes based on comments.

---
+++ src/components/AdminPanel/AdminContentPool/TotalInsurance/TotalInsuranceRow.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { useSubstrate } from '../../../../substrate-lib';
+
+function TotalInsuranceRow({ asset }) {
+	const { api } = useSubstrate();
+	const [totalInsurance, setTotalInsurance] = useState('0');
+
+	const currency = asset;
+
+	const fetchData = async () => {
+		const data = await api.query.liquidityPools.pools(currency);
+		const balance = data.toHuman().total_insurance;
+		setTotalInsurance(balance);
+	};
+	fetchData();
+
+	return <div>{totalInsurance}</div>;
+}
+
+export default TotalInsuranceRow;
--- src/components/AdminPanel/AdminPanel.jsx
+++ src/components/AdminPanel/AdminPanel.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import AdminContentPool from './AdminContentPool/AdminContentPool';
 import InsuranceDeposit from './InsuranceDeposit/InsuranceDeposit';
 import InsuranceRedeem from './InsuranceRedeem/InsuranceRedeem';
 import PoolOperationsStatuses from './PoolOperationsStatuses/PoolOperationsStatuses';
 import PoolOperationsSwitch from './PoolOperationsSwitch/PoolOperationsSwitch';
 
+import classes from './AdminPanel.module.css';
+
 function AdminPanel({ account, setStateStale, stateStale }) {
 	return (
-		<div>
-			<PoolOperationsSwitch account={account} />
-			<PoolOperationsStatuses account={account} />
-			<fieldset>
+		<div className={classes.admin_panel}>
+			<div className={classes.switch}>
+				<PoolOperationsSwitch account={account} />
+				<PoolOperationsStatuses account={account} />
+			</div>
+			<fieldset className={classes.fieldset}>
 				<legend>Insurance operations</legend>
 				<InsuranceDeposit
 					account={account}
@@ -22,6 +27,9 @@ function AdminPanel({ account, setStateStale, stateStale }) {
 					stateStale={stateStale}
 				/>
 			</fieldset>
+			<div className={classes.content}>
+				<AdminContentPool />
+			</div>
 		</div>
 	);
 }
