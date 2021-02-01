bf84605b341163cbbed0a96a1815e97c72ec6842
Polina Liutsko <polina.liutsko@gmail.com>
2021-02-01 17:14:26 +0200

    [FE-167] Implemented proper display of BE values on FE for BalanceUser.

--- src/components/ContentUser/BalanceUser/BalanceUser.jsx
+++ src/components/ContentUser/BalanceUser/BalanceUser.jsx
@@ -1,3 +1,4 @@
+import { formatBalance } from '@polkadot/util';
 import React, { useState } from 'react';
 import { useSubstrate } from '../../../substrate-lib';
 
@@ -5,15 +6,27 @@ function BalanceUser({ account, asset }) {
 	const { api } = useSubstrate();
 	const currency = asset;
 
-	const [currencyBalance, setCurrencyBalance] = useState('0');
+	const [currencyBalance, setCurrencyBalance] = useState('0.0');
 
 	const fetchData = async () => {
 		if (account) {
 			const data = await api.query.tokens.accounts(account, currency);
-			const balance = data.free.toHuman();
+			const decimals = api.registry.chainDecimals;
+			const balanceData = formatBalance(
+				data.free,
+				{ withSi: false, forceUnit: '-' },
+				0
+			)
+				.split('.', 1)
+				.join('')
+				.split(',')
+				.join('');
+			const balance = `${
+				balanceData.slice(0, balanceData.length - decimals) || '0'
+			}.${balanceData.slice(balanceData.length - decimals)}`;
 			setCurrencyBalance(balance);
-		} else if (currencyBalance !== '0') {
-			setCurrencyBalance('0');
+		} else if (currencyBalance !== '0.0') {
+			setCurrencyBalance('0.0');
 		}
 	};
 	fetchData();
