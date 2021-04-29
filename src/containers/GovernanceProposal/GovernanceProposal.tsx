18f74f321fbeb93c0aaa4b2b67127f31ef9d9ce7
Maksym Kalin <maksym.kalin@gmail.com>
2021-04-29 15:05:37 +0400

    [MK/MIN-245] WIP Governance Proposal UI data

--- src/containers/GovernanceProposal/GovernanceProposal.tsx
+++ src/containers/GovernanceProposal/GovernanceProposal.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { Button } from 'semantic-ui-react';
 import {
 	getProposal,
 	resetProposal,
@@ -9,6 +10,7 @@ import {
 } from '../../actions/governanceData';
 import { State } from '../../util/types';
 import Loading from '../../util/Loading';
+import ProposedExtrinsicData from '../../components/Governance/ProposedExtrinsicData/ProposedExtrinsicData';
 
 interface Props {
 	getProposal: (hash: string) => Promise<void>;
@@ -48,8 +50,40 @@ function GovernanceProposal(props: Props) {
 
 	console.log(proposal);
 	console.log(proposalVoting);
+	// TODO already voted ?
 
-	return <div className='governance-proposal-page'>HASH: {proposalHash}</div>;
+	return (
+		<div className='governance-proposal-page'>
+			<div className='proposal-header'>
+				<div className='proposal-hash'>HASH: {proposalHash}</div>
+			</div>
+			<div className='extrinsic'>
+				<ProposedExtrinsicData proposal={proposal} />
+			</div>
+			<div className='proposal-info'>
+				<div className='info-wrapper'>
+					<div className='label'>Threshold:</div>
+					<div className='value'>{proposalVoting.threshold}</div>
+				</div>
+				<div className='info-wrapper'>
+					<div className='label'>Ayes:</div>
+					<div className='value'>{proposalVoting.ayes.length}</div>
+				</div>
+				<div className='info-wrapper'>
+					<div className='label'>Nays:</div>
+					<div className='value'>{proposalVoting.ayes.length}</div>
+				</div>
+				<div className='info-wrapper'>
+					<div className='label'>End (block number):</div>
+					<div className='value'>{proposalVoting.end}</div>
+				</div>
+			</div>
+			<div className='proposal-actions'>
+				<Button>Vote +</Button>
+				<Button>Vote -</Button>
+			</div>
+		</div>
+	);
 }
 
 const mapStateToProps = (state: State) => ({
